155f2588b4b548f60a1aca5d913a6375c0ac70da
RareSpeciment <davidrafael0987@gmail.com>
2025-05-31 11:54:21 +0700

    Update books.php

--- Tugas_Besar/PHP/books.php
+++ Tugas_Besar/PHP/books.php
@@ -69,7 +69,6 @@ try {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -194,5 +193,4 @@ try {
     </script>
     <?php include 'footer.php'; ?>
 </body>
-
 </html>
